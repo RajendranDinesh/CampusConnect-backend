9bd6abd5ec9a808ab8361874c33bacdc5dd8e73b
RajendranDinesh <dinesh2842004@gmail.com>
2024-10-14 09:16:32 +0530

    [ENDORSEMENT] written migrations

---
+++ database/migrations/2024_10_14_034233_create_endorsements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('endorsements', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('skill_id');
+            $table->unsignedBigInteger('endorsed_by');
+            $table->foreign('skill_id')->references('id')->on('skills')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('endorsed_by')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('endorsements');
+    }
+};
