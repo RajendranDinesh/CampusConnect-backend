071c060253313da3c6fa7a6d7330f425f9e3b889
RajendranDinesh <dinesh2842004@gmail.com>
2024-10-14 09:27:48 +0530

    [ExperienceRole] written migrations

---
+++ database/migrations/2024_10_14_035547_create_experience_roles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experience_roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('duration');
+            $table->unsignedBigInteger('exp_id');
+            $table->foreign('exp_id')->references('id')->on('experiences')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experience_roles');
+    }
+};
